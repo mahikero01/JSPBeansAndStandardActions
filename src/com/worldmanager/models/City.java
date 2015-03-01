19e57402d24aa5306e4b63c90d7ea3bdb01e025e
Rico -  Desktop <federicoparassarmiento@yahoo.com>
2015-03-01 21:28:01 +0800

    up

--- src/com/worldmanager/models/City.java
+++ src/com/worldmanager/models/City.java
@@ -1,5 +1,73 @@
 package com.worldmanager.models;
 
-public class City {
+import java.io.Serializable;
+
+public class City implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private int id;
+	private String name;
+	private String countryCode;
+	private String district;
+	private int population;
 	
+	public City() {
+		//do nothing.
+	}
+	
+	public int getId()
+	{
+		return id;
+	}
+	public void setId(int value)
+	{
+		id = value;
+	}
+	//name
+	public String getName()
+	{
+		return name;
+	}
+	public void setName(String value)
+	{
+		name = value;
+	}
+	//countryCode
+	public String getCountryCode()
+	{
+		return countryCode;
+	}
+	public void setCountryCode(String value)
+	{
+		countryCode = value;
+	}
+	//district
+	public String getDistrict()
+	{
+		return district;
+	}
+	public void setDistrict(String value)
+	{
+		district = value;
+	}
+	//population
+	public int getPopulation()
+	{
+		return population;
+	}
+	public void setPopulation(int value)
+	{
+		population = value;
+	}
+	
+	//toString
+	public String toString()
+	{
+		return String.format("Id: %d\tName: %s\tCountry Code: %s\t"
+								+ "District: %s\tPopulation: %d\n"
+							, getId()
+							, getName()
+							, getCountryCode()
+							, getDistrict()
+							, getPopulation());
+	}
 }
